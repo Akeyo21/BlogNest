b7be52b719b774754fa0773072d0fc182b2344e8
Victoria Akeyo <57098529+Akeyo21@users.noreply.github.com>
2025-02-06 15:45:32 +0000

    Convert files to typescript

---
+++ backend/server.ts
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const cors = require('cors')
+const userRoutes = require('./routes/userRoutes')
+const blogRoutes = require('./routes/blogRoutes')
+require('dotenv').config();
+
+
+const app = express();
+
+app.use(cors());
+// middleware, json format
+app.use(express.json());
+
+// connect to db
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        const PORT = process.env.PORT || 4000;
+        app.listen(PORT, () => {
+            console.log(`Connected to db & listening on port ${PORT}`)
+        });
+    })
+    .catch((err:any) => {
+        console.log(err)
+    })
+
+//routes
+app.use('/api/users', userRoutes)
+app.use('/api/blogs', blogRoutes)
